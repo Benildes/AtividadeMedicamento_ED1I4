c757155d58b682b3a703659ef328615ba1859a37
Benildes <54426596+Benildes@users.noreply.github.com>
2022-10-31 19:27:06 -0300

    Add files via upload

---
+++ AtividadeMedicamento/AtividadeMedicamento/Medicamentos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AtividadeMedicamento
+{
+    class Medicamentos
+    {
+        internal readonly object ListaMedicamentos;
+
+        internal Medicamento pesquisar(Medicamento medicamentoprocurado)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void adicionarMedicamento(Medicamento medicamento)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
